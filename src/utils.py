162aaca5ed1beaf7dc2c6087dbf6103fde24700d
ziminyuri <zimin.yuri22@gmail.com>
2022-05-01 16:50:49 +0700

    Добавил пагинацию

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 import logging
 import logging.config
 
+from flask import current_app
 from flask_restful import reqparse
 
 from src.core.logger import LOGGING
@@ -12,7 +13,8 @@ def get_logger(name: str = __name__):
 
 
 def get_pagination_params():
-    parser = reqparse.RequestParser()
-    parser.add_argument('page', type=int, help='Page for pagination')
-    parser.add_argument('number', type=int, help='Number of items for pagination')
-    return parser.parse_args()
+    if not current_app.config['TESTING']:
+        parser = reqparse.RequestParser()
+        parser.add_argument('page', type=int, help='Page for pagination')
+        parser.add_argument('number', type=int, help='Number of items for pagination')
+        return parser.parse_args()
