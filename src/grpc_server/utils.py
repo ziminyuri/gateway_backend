e67a73bc56f5278940f5267b70e0e4d57792b3d6
dmitryvokhmin <boxdima1@gmail.com>
2022-05-05 20:20:32 -0400

    make grpc connection secured

--- src/grpc_server/utils.py
+++ src/grpc_server/utils.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from flask_jwt_extended import decode_token
 from flask_jwt_extended.exceptions import JWTDecodeError
 
@@ -8,3 +10,15 @@ def validate_token(token: str) -> dict:
         raise JWTDecodeError('Token is invalid')
 
     return token
+
+
+def get_credentials():
+    path = Path(__file__).parent
+    with open(path.joinpath('certificates/server.key'), 'rb') as f:
+        server_key = f.read()
+    with open(path.joinpath('certificates/server.pem'), 'rb') as f:
+        server_cert = f.read()
+    with open(path.joinpath('certificates/ca.pem'), 'rb') as f:
+        ca_cert = f.read()
+
+    return server_key, server_cert, ca_cert
